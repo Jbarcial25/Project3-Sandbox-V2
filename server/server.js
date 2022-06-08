3f2232a75be1ae29f3f4a5391691fa1400da498f
Jasper Barcial <Jbarcial25@gmail.com>
2022-06-08 07:28:51 -0700

    fixes

--- server/server.js
+++ server/server.js
@@ -20,17 +20,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "test") {
+    app.use(express.static(path.join(__dirname, "../client/build")))
+  }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+  })
 
-const startApolloServer = async (typeDefs, resolvers) => {
-    await server.start();
-    server.applyMiddleware({ app });
 
-    db.once('open', () => {
-        app.listen(PORT, () => {
-            console.log(`API server running on port ${PORT}!`);
-            console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-        })
+  db.once('open', () => {
+    server.start().then(() => {
+      server.applyMiddleware({
+        app,
+        cors: true
+      })
+      app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`)
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+      })
     })
-};
-
-startApolloServer(typeDefs, resolvers);
+  })
